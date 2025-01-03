36be05e95f27fd4317f886e9bbc451c36b968618
Divyesh Nabhoya <divyeshnabhoya2@gmail.com>
2025-01-03 10:25:37 +0530

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -41,8 +41,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+# ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 
+ALLOWED_HOSTS = ['admin.growitup.co', '64.227.185.138']
 # Application definition
 
 INSTALLED_APPS = [
